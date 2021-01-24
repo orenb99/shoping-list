89e5cfeba9214b31af26dbf7e89bb472226eff90
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-24 13:19:42 +0200

    task 9 done

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const list=document.getElementsByTagName("ul")[0];
+const ulist=document.getElementsByTagName("ul")[0];
 const input=document.getElementsByTagName("input")[0];
-const button=document.getElementsByTagName("button")[0];
-button.setAttribute("onclick","store()");
+const storeButton=document.getElementsByTagName("button")[0];
+storeButton.setAttribute("onclick","store()");
 
 function store(){
     let x=input.value;
     input.value="";
+    const list=document.createElement("li");
+    const span=document.createElement("span");
+    const deleteButton=document.createElement("button");
+    list.appendChild(span);
+    list.appendChild(deleteButton);
+    span.innerText=x;
+    deleteButton.innerText="Delete";
+    ulist.appendChild(list);
+
 }
+
+
+
+
