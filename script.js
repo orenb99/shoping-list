06700acb0a1ba22f975208f542dc6c04effd53a9
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-24 13:03:58 +0200

    task 5 done

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const list=document.getElementsByTagName("ul")[0];
 const input=document.getElementsByTagName("input")[0];
 const button=document.getElementsByTagName("button")[0];
-button.setAttribute("onclick","run()");
-function run(){
+button.setAttribute("onclick","store()");
+
+function store(){
+    let x=input.value;
+    input.value="";
 }
