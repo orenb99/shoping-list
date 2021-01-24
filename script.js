1fd35be8e95912522e56c3e96b2860821592e302
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-24 13:00:41 +0200

    task 3 done

---
+++ script.js
@@ -0,0 +1,6 @@
+const list=document.getElementsByTagName("ul")[0];
+const input=document.getElementsByTagName("input")[0];
+const button=document.getElementsByTagName("button")[0];
+button.setAttribute("onclick","run()");
+function run(){
+}
